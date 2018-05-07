c001792020c91dbbfa92674e35182ebaf9536d2a
Ian Matthew Davidson <idavidson@blue.cs.sonoma.edu>
2018-05-06 18:32:39 -0700

    finished up to address translation

--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 p1.x: p1.cpp
-	g++ p1.cpp -o p1.x
+	g++ -std=c++11 p1.cpp -o p1.x
 
 p2.x: p2.cpp
-	g++ p2.cpp -o p2.x
+	g++ -std=c++11 p2.cpp -o p2.x
 
 clean:
 	rm *~ *.x
--- p1.cpp
+++ p1.cpp
@@ -13,168 +13,105 @@ Date: Spring '18
 
 
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <string>
 #include <cctype>
+#include <iomanip>
+#include <vector>
+#include <math.h>
+
 
 using namespace std;
 
-class TableRep;
-
-TableRep & ReadFile(ifstream & input);
-
-//physAddress parseVirtAddress(int address, bool hex, TableRep * & T);
-
-int main(int argc, char* [] argv)
-{
-	if(argc != 2){ //exe + inputfile
-		cerr << "needs input file as arguement. exiting" << endl;
-		exit(1);
-	}
-	
-	ifstream input (argv[1]);
-	if(!input.good()){
-		cerr << "input is not a file" << endl;
-		exit(1);
-	}
-
-	//make instance of TableRep
-	TableRep * tableRep =  ReadFile(input);
-
-	
-	//now we need to read in from cin, from the file redirected in
-
-	string address = "";
-
-	while(cin >> word)//each result should be a virtual address we need to interpret based off tableRep, 
-	{
-		//address can be binary or hex
-		if(word[1] == 'x'){ //expecting the "0x" in front of the word
-			//hex address
-
-		}else{
-			//decimal address
-
-		}
-		//call parseVirtAddress
-
-	}
-
-
-	return 0;
-}
-
-// parseVirtAddress(int address, bool hex, TableRep * & T){}
-// should return the phsyical address or 0(if permission is )
-
-//should probably use bit shifts maybe shift off the bits of the page to determine value in addr.
-
-
-TableRep & ReadFile(ifstream & input)
-{
-	
-	string word = "";
-
-	//first 3 items are important
-	input >> word;
-
-	//virtBits after casted from string
-	int v = stoi(word);
-
-
-	//first 3 items are important
-	input >> word;
-
-	//physBits after casted from string
-	int p = stoi(word);
-
-
-	//first 3 items are important
-	input >> word;
-
-	//bytesInPage after casted from string
-	int b = stoi(word);
-
-
-	int s = -1; //used an accumulator when figuring out how much input is left from file.
-
-	input.clear();
-	input.seekg(0, ios::beg);
-
-	string line = "";
-
-	//get line
-	while(getline(input,line)){
-		if(!isspace(word)) // a line consisting of information we WANT
-			s++;
-	}
-
-	int ** table;
-
-
-	table = new int * [4];
-
-	for(int i = 0; i < 4; i++){
-		table[i] = new int[s]; 
-	}
-	
-	getline(input, line)//the first 
-
-
-	for(int i = 0; i < s; i++){
-		getline(input, line);
-
-		input >> word;
-		table[0][i] = stoi(word);
-
-		input >> word;
-		table[1][i] = stoi(word);
-
-		input >> word;
-		table[2][i] = stoi(word);
-
-		input >> word;
-		table[3][i] = stoi(word);
-	}
-
-
-	TableRep * tableRep = new TableRep(v, p, b, s, table);
-
-	return *tableRep;
-}
-
-
 class TableRep
 {
-public: 
+public:
+
+
+	class TableEntry{
+	public:
+		TableEntry()
+		{
+			valid = false;
+			permission = false;
+			physicalPageNumber = -1; //so we know its a bad instance
+			useBit = false;//whatever the fuck that it
+		}
+
+		TableEntry(bool v, bool p, int pageNum, bool use)
+		{
+			valid =  v;
+			permission = p;
+			physicalPageNumber = pageNum;
+			useBit = use;
+
+		}
+		~TableEntry()
+		{}
+
+		TableEntry(const TableEntry & t)
+		{
+			valid = t.valid;
+			permission = t.permission;
+			physicalPageNumber = t.physicalPageNumber;
+			useBit = t.useBit;
+		}
+
+		void printEntry(ostream & o) {
+			o << valid << " " << permission << " " << physicalPageNumber << " " << useBit << endl;
+		}
+
+		int getPhysicalPageNumber() {
+			return physicalPageNumber;
+		}
+
+	private:
+		bool valid;
+		bool permission;
+		int physicalPageNumber;
+		bool useBit;//whatever the fuck that it
+		//whatever else we need to keep track of this information.
+	};
+
 	TableRep()
 	{
 		virtBits = 0;
 		physBits = 0;
 		bytesInPage = 0;
-		size = 0;
-		TableRecords = NULL;
+		offset = 0;
+		// bitsInPage
 	}
 
-	TableRep(int v, int p, int b, int s, int ** T){
+	TableRep(int v, int p, int b){
 		virtBits = v;
 		physBits = p;
 		bytesInPage = b;
-		size = s;
-		TableRecords = T;
+		offset = log2(bytesInPage);
+		// bitsInpage = log2(bytesInPage);
+		// TableRecords = other;
 	}
 
 	~TableRep(){
-		delete TableRecords; //only declaring this on the heeep
+		//delete TableRecords; //only declaring this on the heeep
+		for(int i = 0; i < TableRecords.size(); i++ ){
+				delete TableRecords[i];
+		}
+		//need to actually run through array and delete each instane of table entry
 	}
 
-	int ** getTableRecords()
+	void insertTableEntry(bool v, bool p, int pageNum, bool use) {
+		TableEntry* te = new TableEntry( v,  p,  pageNum,  use);
+		TableRecords.push_back(te);
+	}
+
+	vector<TableEntry *> getTableRecords()
 	{
-		return  TableRecords;
+		return TableRecords;
 	}
 
 	int getSize(){
-		return size;
+		return TableRecords.size();
 	}
 
 	int getBytesInPage(){
@@ -189,12 +126,39 @@ public:
 		return physBits;
 	}
 
+	void printTable(ostream & o) {
+		for (int i = 0; i < TableRecords.size(); i++) {
+			TableRecords[i]->printEntry(o);
+		}
+	}
+
+	int translateAddress(int virtualAddress) {
+			// calculate VPN
+			// index the table
+			// offset to find specific address within PPN
+			// return PPN
+
+			int mask = pow(2, virtBits) - 1; // mask of 1s
+			cout << "mask: " << mask << endl;
+			virtualAddress &= mask;
+			cout << "virtual address: " << virtualAddress << endl; // off by 1. 48 instead of 96
+			int VPN = virtualAddress >> offset;
+			cout << "VPN: " << VPN << endl;
+			int PPN = TableRecords[VPN]->getPhysicalPageNumber();
+			int physicalAddress = PPN << offset;
+			mask = pow(2, offset) - 1;
+			physicalAddress &= mask;
+			return physicalAddress;
+	}
+
 private:
-  int virtBits;
-  int physBits;
-  int bytesInPage;
-  int size;
-  int** TableRecords;
+	int virtBits; // bitwidths of the addresses
+	int physBits;
+	int bytesInPage;
+	int offset; // bitwidth of the offset
+	// bitsInpage = log2(bytesInPage);
+	vector<TableEntry*> TableRecords;
+	//TableEntry* TableRecords[];
 /*
 Number of bits in virtual address
 Number of bits in physical address
@@ -203,12 +167,122 @@ The lines of a flat page table for the current process, in the following format:
 Valid?
 Permission (1 bit)
 Physical page number
-Use bit (see Problem 2) 
+Use bit (see Problem 2)
 */
-  
-
-
-
-
-
 };
+
+TableRep * ReadFile(ifstream & input);
+
+//physAddress parseVirtAddress(int address, bool hex, TableRep * & T);
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2){ //exe + inputfile
+		cerr << "needs input file as arguement. exiting" << endl;
+		exit(1);
+	}
+
+	ifstream input (argv[1]);
+	if(!input.good()){
+		cerr << "input is not a file" << endl;
+		exit(1);
+	}
+
+	//make instance of TableRep
+	TableRep * tableRep =  ReadFile(input);
+
+	tableRep->printTable(cout);
+
+	//now we need to read in from cin, from the file redirected in
+
+	string address = "";
+	int addr = -1;
+	int physicalAddress = 0;
+	while(cin >> address)//each result should be a virtual address we need to interpret based off tableRep,
+	{
+		//int addrSize = address.length();
+		addr = -1;
+		physicalAddress = 0;
+		if(address.length() > 1){
+			//either hex or decimal addr
+			 	if(address[1] == 'x'){ //expecting the "0x" in front of the word
+					addr = stoi(address, nullptr, 16);
+		 		}else {
+					addr = stoi(address);
+				}
+		}else{
+			//we know this is specifically a newline char OR a single digit decimal based address
+			if(address[0] != '\n' || address != " ") {
+				addr = stoi(address);
+			}
+		}
+		cout << "virtual addr: " << addr << endl;
+		if(addr < 0) {
+			cout << "invalid address" << endl;
+			return 0;
+		}
+		// process input
+		physicalAddress = tableRep->translateAddress(addr);
+		cout << "phsyical address: " << physicalAddress << endl << endl;
+	}
+
+	return 0;
+}
+
+// parseVirtAddress(int address, bool hex, TableRep * & T){}
+// should return the phsyical address or 0(if permission is )
+
+//should probably use bit shifts maybe shift off the bits of the page to determine value in addr.
+
+
+TableRep * ReadFile(ifstream & input)
+{
+
+	int w = 0;
+
+	//first 3 items are important
+	input >> w;
+
+	//virtBits after casted from string
+	int v = w;
+
+
+	//first 3 items are important
+	input >> w;
+
+	//physBits after casted from string
+	int p = w;
+
+
+	//first 3 items are important
+	input >> w;
+
+	//bytesInPage after casted from string
+	int b = w;
+
+	int s = 0; //used an accumulator when figuring out how much input is left from file.
+	string word = "";
+	int valid;
+	int per;
+	int phy;
+	int use;
+	TableRep* TR = new TableRep(v, p, b);
+
+	while(input >> word) {
+		valid = stoi(word);
+
+		input >> word;
+		per = stoi(word);
+
+		input >> word;
+		phy = stoi(word);
+
+		input >> word;
+		use = stoi(word);
+
+		TR->insertTableEntry(valid, per, phy, use);
+		word = "";
+	}
+
+	return TR;
+}
